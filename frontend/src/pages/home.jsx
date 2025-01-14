b29c76ccec74d00cfbd3ee1e0b2077699ef30c68
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2025-01-14 16:09:04 +0000

    Land page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <>
         <body className="bg-landing_img h-full w-full bg-no-repeat bg-cover lg:bg-black">
             <NavBar/>
-            <div className="flex flex-col justify-center border-2 border-red-500 h-svh">
+            <div className="flex flex-col justify-center border-2 border-red-500">
                 <div className="bg-landing_img w-auto m-10 h-2/6 bg-no-repeat bg-cover"></div>
                 <p className="m-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus reprehenderit quae pariatur recusandae tempora, placeat ad doloremque laboriosam vero et fugiat esse cum vitae temporibus iste labore! Possimus, a dolorum!</p>
             </div>
